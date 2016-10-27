47c8f29db72528e23bdf9ee291bd8e001c61a198
Michael Jones <mjclackjones@gmail.com>
2016-10-27 17:30:17 +0100

    Complete example contrasting differential expression analysis that includes venn diagram to show similarly up-regulated genes in two contrasting experiments

--- arrayexpress_find_tnbc.R
+++ arrayexpress_find_tnbc.R
@@ -8,9 +8,25 @@ if (!is.tcga.biolinks.installed) {
 library('ArrayExpress')
 sets <- queryAE(keywords = 'triple-negative+breast+cancer', species= 'homo+sapiens')
 
-set.ids <- as.character(sets$ID)[2]
-set.data <- getAE(set.ids, type = "processed")
-
-# set.data <- lapply(set.ids, getAE, type = full)
+set.ids <- as.character(sets$ID)
 
 
+# for one - need to do for N
+# also - not tnbc data so need to think of way to get this
+set.data <- ArrayExpress(set.ids[2])
+fac.column <- grep("FactorValue",colnames(pData(set.data)), value=T)
+facs <- pData(set.data)[,fac.column]
+
+fac <- factor(facs)
+design <- model.matrix(~0+fac)
+colnames(design) <- c('cancer.assoc.fibroblast', 'granulin.stimulated.fibroblast', 'normal.mammary.tissue')
+fit <- lmFit(set.data, design)
+
+cont.matrix <- makeContrasts(cancerFvsNormalT=cancer.assoc.fibroblast-normal.mammary.tissue,granulinFvsNormalT=granulin.stimulated.fibroblast-normal.mammary.tissue, levels=design)
+fit2  <- contrasts.fit(fit, cont.matrix)
+fit2 <- eBayes(fit2)
+
+res.cancer.vs.normaltissue <- topTable(fit2, coef = 'cancerFvsNormalT', adjust="BH")
+
+results  <- classifyTestsF(fit2, p.value=0.0001)
+vennDiagram(results, include="up")
